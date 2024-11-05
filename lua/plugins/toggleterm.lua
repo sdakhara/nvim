5b678e1875d31366ff96f3472ff338086139a8da
sdakhara <scdakhara123@gmail.com>
2024-11-05 22:50:25 +0530

    removed toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,12 +1,1 @@
-return {
-  'akinsho/toggleterm.nvim',
-  version = "*",
-  config = function()
-    require("toggleterm").setup {
-      -- Customize your terminal settings here
-      open_mapping = [[<leader>t]], -- Use Ctrl+t to toggle the terminal
-      direction = 'horizontal',     -- Change this to 'vertical' or 'float' if desired
-      size = 20,                    -- Size of the terminal window
-    }
-  end
-}
+return {}
