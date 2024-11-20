fc0166f3633e844de601e130a7266b855d825cad
sdakhara <scdakhara123@gmail.com>
2024-11-20 23:36:06 +0530

    added lazygit configs

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -1,16 +1,29 @@
 return {
 	"akinsho/toggleterm.nvim",
+	version = "*", -- Install the latest stable version
 	config = function()
-		require("toggleterm").setup({
-			size = 20,
-			open_mapping = [[<C-\>]],
-			direction = "float",
+		require("toggleterm").setup {
+			open_mapping = [[<c-\>]], -- Keybinding to open a terminal
+			direction = "float",     -- Open terminal in floating mode
 			float_opts = {
-				border = "curved",
+				border = "curved",     -- Border style for the floating window
+				winblend = 0,          -- Transparency (0 means no transparency)
+				highlights = {
+					border = "FloatBorder", -- Match border color to Normal highlight
+					background = "Normal", -- Match background to Normal highlight
+				},
+
 			},
-		})
-		vim.keymap.set('n', '<leader>gg', function()
-			vim.cmd("ToggleTerm direction=float cmd='lazygit'")
-		end, { desc = "Open Lazygit" })
-	end
+		}
+
+		-- Function to toggle LazyGit
+		function _lazygit_toggle()
+			local Terminal = require("toggleterm.terminal").Terminal
+			local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+			lazygit:toggle()
+		end
+
+		-- Keybinding for LazyGit
+		vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+	end,
 }
