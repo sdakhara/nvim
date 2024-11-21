7f5b56282cef7cd6cc577cc4385288f5fc781bb3
sdakhara <scdakhara123@gmail.com>
2024-11-21 05:02:30 +0000

    finished lsp config and added telescope select ui and added code actions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,6 @@
 return {
 
-	{    
+	{
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup({})
@@ -11,6 +11,52 @@ return {
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {"lua_ls"}
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local mason_lspconfig = require("mason-lspconfig")
+
+			-- Set default `on_attach` and `capabilities` for all language servers
+			local on_attach = function(_, bufnr)
+				local opts = { noremap = true, silent = true, buffer = bufnr }
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+			end
+
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+			-- Automatically configure language servers installed by Mason
+			mason_lspconfig.setup_handlers({
+				-- Default handler for all servers
+				function(server_name)
+					lspconfig[server_name].setup({
+						on_attach = on_attach,
+						capabilities = capabilities,
+					})
+				end,
+
+				-- Specific settings for lua_ls (example)
+				["lua_ls"] = function()
+					lspconfig.lua_ls.setup({
+						on_attach = on_attach,
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								diagnostics = { globals = { "vim" } },
+								workspace = {
+									library = vim.api.nvim_get_runtime_file("", true),
+									checkThirdParty = false,
+								},
+							},
+						},
+					})
+				end,
 			})
 		end
 	}
