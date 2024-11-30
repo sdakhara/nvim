1454b4eaec86c86e1e4d76fd7ae023e95605d981
sdakhara <scdakhara123@gmail.com>
2024-11-30 04:00:52 +0000

    added config to show the hidden files all time

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,4 +1,4 @@
-return	{
+return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
 	dependencies = {
@@ -8,11 +8,19 @@ return	{
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
 	config = function()
-		vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<CR>', { desc = "Toggle Neo-tree" })
-		vim.keymap.set('n', '<leader>o', '<cmd>Neotree focus<CR>', { desc = "Focus Neo-tree" })
-		vim.keymap.set('n', '<leader>r', '<cmd>Neotree refresh<CR>', { desc = "Refresh Neo-tree" })
-		vim.keymap.set('n', '<leader>n', '<cmd>Neotree reveal<CR>', { desc = "Reveal Current File in Neo-tree" })
-		vim.keymap.set('n', '<leader>c', '<cmd>Neotree close<CR>', { desc = "Close Neo-tree" })
-	end
+		vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+		vim.keymap.set("n", "<leader>o", "<cmd>Neotree focus<CR>", { desc = "Focus Neo-tree" })
+		vim.keymap.set("n", "<leader>r", "<cmd>Neotree refresh<CR>", { desc = "Refresh Neo-tree" })
+		vim.keymap.set("n", "<leader>n", "<cmd>Neotree reveal<CR>", { desc = "Reveal Current File in Neo-tree" })
+		vim.keymap.set("n", "<leader>c", "<cmd>Neotree close<CR>", { desc = "Close Neo-tree" })
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true, -- Show hidden files by default
+					hide_dotfiles = false, -- Do not hide dotfiles
+					hide_gitignored = false, -- Optionally show .gitignored files too
+				},
+			},
+		})
+	end,
 }
-
