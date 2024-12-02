5f1a206c97187404c902c9c40fa8ea59f8e1d2ad
sdakhara <scdakhara123@gmail.com>
2024-12-02 06:41:39 +0000

    Added whichwrap
    Now whenever the cursor will be at end of the line and you press l it
    will go the first char of the next line and same for the h

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,9 +6,28 @@ vim.opt.shiftwidth = 4
 vim.keymap.set("i", "jj", "<Esc>")
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.whichwrap:append("<,>,h,l")
 
-vim.keymap.set('i', '<A-h>', '<Left>', { desc = 'Move left in insert mode' })
+vim.keymap.set('i', '<A-h>', function()
+    local col = vim.fn.col(".")
+    if col == 1 then
+        return "<C-o>k<C-o>$" -- Move up to previous line and end
+    else
+        return "<Left>" -- Move left within the current line
+    end
+end, { expr = true, desc = 'Move left or wrap to previous line in insert mode' })
+
 vim.keymap.set('i', '<A-j>', '<Down>', { desc = 'Move down in insert mode' })
+
 vim.keymap.set('i', '<A-k>', '<Up>', { desc = 'Move up in insert mode' })
-vim.keymap.set('i', '<A-l>', '<Right>', { desc = 'Move right in insert mode' })
+
+vim.keymap.set('i', '<A-l>', function()
+    local col = vim.fn.col(".")
+    local line = vim.fn.getline(".")
+    if col > #line then
+        return "<C-o>j<C-o>0" -- Move to next line and to its beginning
+    else
+        return "<Right>" -- Move right within the current line
+    end
+end, { expr = true, desc = 'Move right or wrap to next line in insert mode' })
 
